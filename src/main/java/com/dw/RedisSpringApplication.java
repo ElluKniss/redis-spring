a6e50c6cd13542b666ffa84c275902625a9abf89
ElluKniss <dai21wei@163.com>
2022-10-28 13:44:04 +0800

    first提交修改

--- src/main/java/com/dw/RedisSpringApplication.java
+++ src/main/java/com/dw/RedisSpringApplication.java
@@ -2,12 +2,18 @@ package com.dw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisSpringApplication.class, args);
+    }
+
+    @Bean
+    public Object cc(){
+        return new Object();
     }
 
 }
